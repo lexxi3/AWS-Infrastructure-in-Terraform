1716b36a95a0e82e4e833978fbef294233c48aec
Alexia  Ruiz <alexia.ruiz@clickittech.com.mx>
2023-06-21 15:57:43 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
         region = "us-west-2"
-        access_key = "AKIA5J5TBRUF7ZXLJI53"
-        secret_key = "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"
+        access_key = "AKXXXXXXXXXXXXXXXXXI53"
+        secret_key = "kmrBXXxXxxXxxXXXxxxXXxWkZCNs"
 }
